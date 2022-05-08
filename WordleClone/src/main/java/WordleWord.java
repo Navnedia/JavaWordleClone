2ffcc2da873b1df28e91a720411f455ac1ba0e01
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-08 01:54:49 -0500

    Changed structure and started implementing some logic
    Changed to using string instead of char array because I think this
    should make guess evaluation easier.
    
    Created a basic first pass for the code to evaluate a guess against the
    WordleWord object.

--- WordleClone/src/main/java/WordleWord.java
+++ WordleClone/src/main/java/WordleWord.java
@@ -3,25 +3,74 @@ import java.util.Arrays;
 
 public class WordleWord implements Serializable {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -5748366559941088303L;
 	
-	public char[] wordValue;
+	public String wordValue;
 	
 	
 	public WordleWord(String word) {
-		this.wordValue = word.toCharArray();
-	}
-	
-	public WordleWord(char[] word) {
 		this.wordValue = word;
 	}
 	
+	/**
+	 * Compares an input word against this {@code WordleWord} object.
+	 * Simple first pass for evaluating. More complex logic down the line.
+	 * 
+	 * @param guess the {@code String} word to evaluate against.
+	 * @return A {@code int[]} with the evaluations of each index/letter of the word.
+	 * 		   0 when the letter is in the word AND in the correct spot.
+	 * 		   1 when the letter is in the word BUT in the wrong spot.
+	 *  	   -1 when the letter is NOT in the word.
+	 */
+	public int[] evaluate(String guess) {
+		// Guard statement to protect against bad inputs:
+		if (guess == null || guess.length() != wordValue.length()) { throw new IllegalArgumentException();}
+		
+		char[] guessChars = guess.toCharArray();
+		int[] evaluations = new int[wordValue.length()];
+		
+		for (int i = 0; i < guessChars.length; i++) {
+			int location = wordValue.indexOf(guessChars[i]);
+			
+			if (location == i) {
+				evaluations[i] = WordleGame.CORRECT;
+			} else if (location >= 0) {
+				evaluations[i] = WordleGame.PRESENT;
+			} else {
+				evaluations[i] = WordleGame.ABSENT;
+			}
+		}
+		
+		return evaluations;
+	}
+	
+	public static String[] translateEvaluation(int[] numEvaluation) {
+		String[] strEvaluation = new String[numEvaluation.length];
+		
+		for (int i = 0; i < strEvaluation.length; i++) {
+			switch (numEvaluation[i]) {
+			case WordleGame.CORRECT:
+				strEvaluation[i] = "correct";
+				break;
+			case WordleGame.PRESENT:
+				strEvaluation[i] = "present";
+				break;
+			case WordleGame.ABSENT:
+				strEvaluation[i] = "absent";
+				break;
+			default:
+				throw new IllegalArgumentException("Input contains an invalid evaluation int");
+			}
+		}
+		
+		return strEvaluation;
+	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + Arrays.hashCode(wordValue);
+		result = prime * result + ((wordValue == null) ? 0 : wordValue.hashCode());
 		return result;
 	}
 
@@ -31,10 +80,27 @@ public class WordleWord implements Serializable {
 		if (obj == null) {return false;}
 		if (getClass() != obj.getClass()) {return false;}
 		
-		WordleWord other = (WordleWord) obj;
-		if (!Arrays.equals(wordValue, other.wordValue)) {return false;}
+		WordleWord otherWord = (WordleWord) obj;
+		if (wordValue == null) {
+			if (otherWord.wordValue != null) {return false;}
+		} else if (!wordValue.equals(otherWord.wordValue)) {
+			return false;
+		}
 		
 		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		WordleWord answer = new WordleWord("midst");
+		
+		int[] evaluation = answer.evaluate("title");
+		
+		int[] test = {1, 0, -1, 0, 3};
+		
+		System.out.println(Arrays.toString(translateEvaluation(evaluation)));
+		
+		System.out.println(Arrays.toString(translateEvaluation(test)));
 	}
 	
 }
