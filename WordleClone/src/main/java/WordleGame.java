c98ed95cc893c258b7206f46865b3a682e1d4edf
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-08 01:50:17 -0500

    Change some of the structure

--- WordleClone/src/main/java/WordleGame.java
+++ WordleClone/src/main/java/WordleGame.java
@@ -1,7 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
 
-public class WordleGame {
+public class WordleGame  implements Serializable {
+
+	private static final long serialVersionUID = -61192559081134857L;
 	
 	/**
 	 * Here are some ideas, some might be redundant like the board and the guesses.
@@ -9,43 +10,47 @@ public class WordleGame {
 	 * Maybe the wordleWords show be 
 	 */
 	private WordleWord answer;
-	private char[][] board;
-	private List<WordleWord> guesses;
-	private int wordLength;
-	private int maxAttempts;
+	private String[][] boardEvaluations; // "correct", "present", or "absent".
+	private WordleWord[] guesses;
+	static final int WORD_LENGTH = 5;
+	static final int MAX_ATTEMPTS = 6;
 	
-	public WordleGame(List<WordleWord> words) {
-		guesses = new ArrayList<WordleWord>(words);
+	// Letter Evaluations:
+	static final int CORRECT = 0; // Letter is in the word AND in the correct spot.
+	static final int PRESENT = 1; // Letter is in the word BUT in the wrong spot.
+	static final int ABSENT = -1; // Letter is NOT in the word.
+	
+	
+	public WordleGame(WordleWord[] words) {
+		this.guesses = words;
 	}
 	
 	// This is just playing around:
-	public void printboard() {
+	public void printBoard() {
 		StringBuilder sb = new StringBuilder();
 		
 		for (WordleWord wordleWord : guesses) {
 			sb.setLength(0); // Clear String Builder.
 			
 			// Construct Letter String:
-			for (char letter : wordleWord.wordValue) {
+			for (char letter : wordleWord.wordValue.toCharArray()) {
 				// Maybe I can some how integrate the colors into the letter like having a letter class
 				sb.append("[" + ConsoleColor.GREEN + letter + ConsoleColor.RESET + "]");
 			}
-			System.out.println(sb.toString());
+			System.out.println(sb.toString() + "\n");
 		}
 	}
 	
 	public static void main(String[] args) {
-		List<WordleWord> words = new ArrayList<WordleWord>();
-		words.add(new WordleWord("orate"));
-		words.add(new WordleWord("hello"));
-		words.add(new WordleWord("balls"));
-		words.add(new WordleWord("fudge"));
-		words.add(new WordleWord("dodge"));
+		WordleWord[] words = {new WordleWord("orate"),
+							  new WordleWord("hello"),
+							  new WordleWord("balls"),
+							  new WordleWord("fudge"),
+							  new WordleWord("dodge")};
 		
 		WordleGame wg = new WordleGame(words);
 		
-		wg.printboard();
-		
+		wg.printBoard();
 	}
 
 }
