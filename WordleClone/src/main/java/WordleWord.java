c09139dae58ab45c03d710af042dbe045402fc86
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-08 02:17:16 -0500

    Updated evaluations to return/use a byte[] instead of an int[]

--- WordleClone/src/main/java/WordleWord.java
+++ WordleClone/src/main/java/WordleWord.java
@@ -1,6 +1,11 @@
 import java.io.Serializable;
 import java.util.Arrays;
 
+import javax.swing.JOptionPane;
+
+/**
+ * @author Navnedia
+ */
 public class WordleWord implements Serializable {
 	
 	private static final long serialVersionUID = -5748366559941088303L;
@@ -17,17 +22,17 @@ public class WordleWord implements Serializable {
 	 * Simple first pass for evaluating. More complex logic down the line.
 	 * 
 	 * @param guess the {@code String} word to evaluate against.
-	 * @return A {@code int[]} with the evaluations of each index/letter of the word.
+	 * @return A {@code byte[]} with the evaluations of each index/letter of the word.
 	 * 		   0 when the letter is in the word AND in the correct spot.
 	 * 		   1 when the letter is in the word BUT in the wrong spot.
 	 *  	   -1 when the letter is NOT in the word.
 	 */
-	public int[] evaluate(String guess) {
+	public byte[] evaluate(String guess) {
 		// Guard statement to protect against bad inputs:
 		if (guess == null || guess.length() != wordValue.length()) { throw new IllegalArgumentException();}
 		
 		char[] guessChars = guess.toCharArray();
-		int[] evaluations = new int[wordValue.length()];
+		byte[] evaluations = new byte[wordValue.length()];
 		
 		for (int i = 0; i < guessChars.length; i++) {
 			int location = wordValue.indexOf(guessChars[i]);
@@ -44,11 +49,11 @@ public class WordleWord implements Serializable {
 		return evaluations;
 	}
 	
-	public static String[] translateEvaluation(int[] numEvaluation) {
-		String[] strEvaluation = new String[numEvaluation.length];
+	public static String[] translateEvaluation(byte[] origEvaluation) {
+		String[] strEvaluation = new String[origEvaluation.length];
 		
 		for (int i = 0; i < strEvaluation.length; i++) {
-			switch (numEvaluation[i]) {
+			switch (origEvaluation[i]) {
 			case WordleGame.CORRECT:
 				strEvaluation[i] = "correct";
 				break;
@@ -94,13 +99,9 @@ public class WordleWord implements Serializable {
 	public static void main(String[] args) {
 		WordleWord answer = new WordleWord("midst");
 		
-		int[] evaluation = answer.evaluate("title");
-		
-		int[] test = {1, 0, -1, 0, 3};
+		byte[] evaluation = answer.evaluate("title");
 		
 		System.out.println(Arrays.toString(translateEvaluation(evaluation)));
-		
-		System.out.println(Arrays.toString(translateEvaluation(test)));
 	}
 	
 }
